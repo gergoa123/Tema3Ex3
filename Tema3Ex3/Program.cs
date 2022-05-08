3532af1dd50457c9ed62163d938b66323cd5ad4f
Gergoa123 <gergo3124@gmail.com>
2022-05-08 15:06:41 +0300

    Add project files.

---
+++ Tema3Ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tema3Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul!");
+            double numar = int.Parse(Console.ReadLine());
+            double aux = numar;
+
+            numar = (Math.Sqrt(numar));
+
+             if (numar * numar == aux)
+             {
+                Console.WriteLine("Este patrat perfect");
+             }
+             else
+            {
+                Console.WriteLine("Nu este patrat perfect");
+            }
+
+
+        }
+    }
+}
